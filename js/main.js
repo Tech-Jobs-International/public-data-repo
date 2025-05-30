2cf6eff4a193f6885b2b710ca9ef755c634bbf13
SoloBaChi <solobachi02@gmail.com>
2025-05-31 00:15:42 +0100

    integrated logic for sorting all the csv data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 
+const defaulltURL = `data/development-financing-institutions-lac`
+
+const datasets = [
+  {
+    title: "All Development Banks Data",
+    path: `${defaulltURL}/LAC_All_Developnment_Banks.csv`,
+  },
+  {
+    title: "Multilateral Development Banks Data",
+    path: `${defaulltURL}/LAC_MDBS.csv`,
+  },
+  {
+    title: "National Development Bank Data",
+    path: `${defaulltURL}/LAC_NDBS.csv`,
+  },
+  {
+    title: "Sovereign Wealth Fund Data",
+    path: `${defaulltURL}/LAC_SWF.csv`,
+  },
+  {
+    title: "Public Procurement Spending in LAC",
+    path: "data/prublic procurement and spending/public procurement spending in LAC.csv",
+  }
+];
+
+
+
+
+
+
 let table;
 const noResultsMsg = document.getElementById("no-results");
 
@@ -61,9 +91,14 @@ function decodeText(text) {
 
 
 // Initialize table after DOM load
-document.addEventListener("DOMContentLoaded", async function () {
+async function loadDataset(dataset) {
+
+  console.log(dataset.title);
+
+  document.getElementById("dataset-title").textContent = dataset.title;
     try {
-      const response = await fetch("data/development-financing-institutions-lac/LAC_SWF.csv");
+      console.log(dataset?.path)
+      const response = await fetch(dataset?.path);
       const buffer = await response.arrayBuffer();
       const decoder = new TextDecoder("windows-1252");
       const csvText = decoder.decode(buffer);
@@ -83,6 +118,8 @@ document.addEventListener("DOMContentLoaded", async function () {
             headerSort: true,
             headerTooltip: true,
           }));
+
+          if (table) table.destroy(); // remove old table
   
           table = new Tabulator("#table-container", {
             data,
@@ -100,7 +137,32 @@ document.addEventListener("DOMContentLoaded", async function () {
     } catch (error) {
       console.error("Failed to load and parse CSV:", error);
     }
+
+}
+
+
+
+// Render Tabs
+function initTabs() {
+  const tabsContainer = document.getElementById("tabs");
+  datasets.forEach((dataset, index) => {
+    const tab = document.createElement("button");
+    tab.textContent = dataset.title;
+    tab.className = `px-4 py-2 text-sm border-b-2 ${index === 0 ? 'border-[#49d94b] font-bold' : 'border-transparent'}`;
+    tab.addEventListener("click", () => {
+      document.querySelectorAll("#tabs button").forEach(btn => btn.classList.remove("border-[#49d94b]", "font-bold"));
+      tab.classList.add("border-[#49d94b]", "font-bold");
+      loadDataset(dataset)
+    });
+    tabsContainer.appendChild(tab);
   });
+
+  loadDataset(datasets[0]); // Load first tab by default
+}
+
+initTabs();
+
+
 
 
 // Download CSV button
